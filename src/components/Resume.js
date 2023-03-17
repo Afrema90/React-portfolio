15f8b64ab9e863a51caa956ac953b977f8d55497
Ama Frema <frema90@gmail.com>
2023-03-16 20:27:10 -0400

    updated

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,39 +1,19 @@
-import React from "react";
+// import React from 'react';
 
-function Resume() {
+const Resume = () => {
+    const downloadResume = () => {
+      const link = document.createElement('a');
+      link.href = '/resume (0035) 2.pdf';
+      link.download = 'my_resume.pdf';
+      link.click();
+    };
+  
     return (
-        <section className="resume-section" id="resume">
-        <div className="resume-section-content">
-            <h1 className="mb-0">
-            Ama
-            <span className="text-primary">Frema</span>
-            </h1>
-            <div className="subheading mb-5">
-            97 N Main St · Boylston, MA 01583 · (860) 816-1613 ·
-            <a href={process.env.PUBLIC_URL + "/ama-frema-resume.pdf"}
-            target="_blank" rel="noreferrer"
-            >
-                <span className="icon">
-                    <i className="fas fa-download"></i>
-                </span>
-                <span>Download Resume</span>
-            </a>
-            <div className="column">
-                <p className="content is-medium">Skills</p>
-                <hr />
-                <ul>
-                    <li>HTML, CSS, Javascript</li>
-                    <li>React, Node.js, Express.js, MongoDB, MySQL,JSON</li>
-                    <li>Bootstrap, Phython, Nosql,firebase,restapi,Cloudinary</li>
-                    <li>Git, Github, Heroku, VS Code, Slack</li>
-                </ul>
-                {/* </ul> */}
-            </div>
-        </div>
-        </div>
-        </section>
-        );
-
-        }
-
-        export default Resume;
+      <div>
+        <h1>My Resume</h1>
+        <button onClick={downloadResume}>Download</button>
+      </div>
+    );
+  };
+  
+  export default Resume;
